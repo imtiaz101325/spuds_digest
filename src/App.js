530c6fe5eba5b3fc834083401b3bd55ebbdaacfe
Sk. Imtiaz Ahmed <excalliburbd@gmail.com>
2018-04-26 21:30:09 +0600

    added react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
-
 import { Provider } from "react-redux";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import store from "./store";
 
+import Home from "./HomePage";
+
 const App = () => (
   <Provider store={store}>
-    <div>Spud's Digest</div>
+    <BrowserRouter >
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </BrowserRouter>
   </Provider>
 );
 
